fa88c8e7510a00916f259bc4ae2273a1112ec6fc
Eugene Levinson <snow-bug@yandex.ru>
2022-02-09 18:44:01 +0000

    added a count down to deadline. No submissions after the deadline are possible now

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const gender_list = ["man", "woman", "nonbinary", "rather not say", "foo", "bar"]
-expected_questions = 3
+event_deadline = "2022-02-14T00:00"
+event_post = "2022-02-14T10:00"
 
-module.exports = {gender_list}
+module.exports = {gender_list, event_deadline, event_post}
